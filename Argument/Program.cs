4f1cff678425d67756723536123bdb2744bb67b3
Mucaht <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-07 15:57:35 +0300

    new

---
+++ Argument/Program.cs
@@ -0,0 +1,12 @@
+﻿class program
+{
+    static void Main(string[] CmdArgs)
+    {
+    Console.WriteLine("Total Arguments: {0}",CmdArgs.Length);
+
+    Console.Write("Arguments: ");
+
+    foreach(var arg in CmdArgs)
+            Console.Write(arg + ", ");
+    }
+}
---
+++ params/params.cs
@@ -0,0 +1,26 @@
+﻿class program
+{
+    static void Main()
+    {
+        int sum1 = SumParameters(1);
+        int sum2 = SumParameters(1,2);
+        int sum3 = SumParameters(1,2,3);
+        int sum4 = SumParameters(4,6,8,2);
+
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+        Console.WriteLine(sum3);
+        Console.WriteLine(sum4);
+
+    }
+    static int SumParameters(params int[] values)
+    {
+        //toplama işlemi
+        int sum = 0;
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+        return sum;
+    }
+}
