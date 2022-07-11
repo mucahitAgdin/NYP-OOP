52545bd3c5e80bd46c2f0c64aa505de872fa373f
Mucaht <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-11 10:41:40 +0300

    game

--- Argument/Program.cs
+++ Argument/Program.cs
@@ -2,11 +2,11 @@
 {
     static void Main(string[] CmdArgs)
     {
-    Console.WriteLine("Total Arguments: {0}",CmdArgs.Length);
+        Console.WriteLine("Total Arguments: {0}", CmdArgs.Length);
 
-    Console.Write("Arguments: ");
+        Console.Write("Arguments: ");
 
-    foreach(var arg in CmdArgs)
+        foreach (var arg in CmdArgs)
             Console.Write(arg + ", ");
     }
 }
