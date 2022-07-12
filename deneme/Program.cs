f5d858dc95f7038268d3e69a3a2e0a73ce99ac86
Mücahit <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-12 16:12:18 +0300

    deneme

--- deneme/Program.cs
+++ deneme/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace deneme
 {
+    class Ogrenci
+    {
+        public int ogrenciNo;
+        public string adSoyad;
+
+        public Ogrenci(int _ogrenciNo, string _adSoyad)
+        {
+            ogrenciNo = _ogrenciNo;
+            adSoyad = _adSoyad;
+        }
+    }
+    class Ogrenciler
+    {
+        List<Ogrenci> liste = new List<Ogrenci>();
+
+        public void add(Ogrenci ogr)
+        {
+            liste.Add(ogr);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
