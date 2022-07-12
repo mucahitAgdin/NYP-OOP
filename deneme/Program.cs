62f274172f994c5c050524b774577715720123a7
Mücahit <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-12 20:37:35 +0300

    deneme

--- deneme/Program.cs
+++ deneme/Program.cs
@@ -22,12 +22,39 @@ namespace deneme
         {
             liste.Add(ogr);
         }
+        public Ogrenci get_Ogrenci(int i)
+        {
+            if (i >= 0 && i < get_len())
+                return liste[i];
+            else
+                return null;
+        }
+        public int get_len()
+        {
+            return liste.Count;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Ogrenciler nesne01 = new Ogrenciler();
+            nesne01.add(new Ogrenci(234, "mehmet"));
+            nesne01.add(new Ogrenci(15, "başak"));
+            nesne01.add(new Ogrenci(2, "emre"));
+            nesne01.add(new Ogrenci(23, "muco"));
 
+            Ogrenci nesne_ogr = nesne01.get_Ogrenci(2);
+            Console.WriteLine(nesne_ogr.adSoyad + ", " + nesne_ogr.ogrenciNo);
+
+            for (int i = 0; i < nesne01.get_len(); i++)
+            {
+                nesne_ogr = nesne01.get_Ogrenci(i);
+                    Console.WriteLine(nesne_ogr.adSoyad + ", " + nesne_ogr.ogrenciNo);   
+            }
+            nesne_ogr = nesne01.get_Ogrenci(2);
+            if(nesne_ogr != null)
+                Console.WriteLine(nesne_ogr.adSoyad + ", " + nesne_ogr.ogrenciNo);
         }
     }
 }
