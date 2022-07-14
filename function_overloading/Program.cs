253d583cd11ffa7e93287daf0bef3f47cccc5f6a
Mücahit <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-14 10:36:51 +0300

    function_overloading

---
+++ function_overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace function_overloading
+{
+    public class islem
+    {
+        public int topla()
+        {
+            return 0;
+        }
+        public int topla(int i)
+        {
+            return i;
+        }
+        public int topla(int i, int j)
+        {
+            return i + j;
+        }
+        public int topla(int i, int j, int k)
+        {
+            return i + j + k;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            islem toplam = new islem();
+            Console.WriteLine("topla1: "+toplam.topla()+ "\ntopla2: "+ toplam.topla(1)+ "\ntopla3: "+ toplam.topla(1,2)+"\ntopla4: "+ toplam.topla(1,2,3));
+        }
+    }
+}
