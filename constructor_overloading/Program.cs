c1002aa1860c7da89b50e2aab26e487918d1493e
Mücahit <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-15 14:23:15 +0300

    new

---
+++ constructor_overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace constructor_overloading
+{
+    public class overload
+    {
+        private int i;
+        public overload()
+        {
+            i = 0;
+        }
+        public overload(int x)
+        {
+            i = x;
+        }
+        public overload(int y, int z)
+        {
+            i = y + z;
+        }
+        public int m_get()
+        {
+            return i;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            overload nesne0 = new overload();
+            Console.WriteLine("not params: " + nesne0.m_get());
+            overload nesne1 = new overload(4);
+            Console.WriteLine("not params: " + nesne1.m_get());
+            overload nesne2 = new overload(3, 5);
+            Console.WriteLine("not params: " + nesne2.m_get());
+        }
+    }
+}
--- function_overloading/Program.cs
+++ function_overloading/Program.cs
@@ -2,6 +2,7 @@
 
 namespace function_overloading
 {
+    //aynı türlerde fakat parametre sayıları farklı 
     public class islem
     {
         public int topla()
@@ -20,13 +21,24 @@ namespace function_overloading
         {
             return i + j + k;
         }
+        //aynı parametreli farklı türlerde function overloading
+        public int carp(int x,int y)
+        {
+            return x*y;
+        }
+        public int carp(string x,string y)
+        {
+            return Convert.ToInt32(x)*Convert.ToInt32(y);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            islem carpim = new islem();
             islem toplam = new islem();
-            Console.WriteLine("topla1: "+toplam.topla()+ "\ntopla2: "+ toplam.topla(1)+ "\ntopla3: "+ toplam.topla(1,2)+"\ntopla4: "+ toplam.topla(1,2,3));
+            Console.WriteLine("topla1: " + toplam.topla() + "\ntopla2: " + toplam.topla(1) + "\ntopla3: " + toplam.topla(1, 2) + "\ntopla4: " + toplam.topla(1, 2, 3));
+            Console.WriteLine("\ncarp_int: "+ carpim.carp(2,5)+"\ncarp_string: "+carpim.carp('x','y'));
         }
     }
 }
